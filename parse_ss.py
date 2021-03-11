1720d8284e2069acf1477730cabdf07b4a1f0b19
Emilia Ndilokelwa Weyulu <eweyulu@mpi-inf.mpg.de>
2021-03-11 11:43:34 +0100

    Corrections for saving log results

--- parse_ss.py
+++ parse_ss.py
@@ -6,7 +6,6 @@ Parse ss logs
 """
 
 import os
-import json
 import pandas as pd
 import re
 
@@ -20,15 +19,11 @@ def __read_sslog(logfile):
         The next entry in file f. None if there is no entry.
     """
     with open(logfile, 'r') as f:
-        
-        parameters = ['minrtt', 'retrans', 'rtt', 'rto', 'data_segs_out', 
-                  'cwnd', 'unacked', 'bbr_pacing_gain', 'bbr_minrtt', 
-                  'bbr_bw', 'bbr_cwnd_gain', 'bytes_acked', 'pacing_rate']
-        
-        results = pd.DataFrame(columns=parameters)
     
-        data = {}
+        lst = []
         for line in f:
+            data = {}
+            # data = {}
             
             # line = f.readline()
             # if not line:
@@ -44,74 +39,79 @@ def __read_sslog(logfile):
                 
                 port = line.strip()
                 port = int(port[port.rfind(':') + 1:])
-                data['port'] = port
+                # data['port'] = port
                 continue
            
             if not line:
                 return None
-            data[time] = {}
+            data['time'] = time
+            data['port'] = port
             stat = line.strip().split()
             cc = stat[0]
             
-            data[time][cc] = {}
+            # filter_lst = ['wscale, mss, segs_out, segs_in, send, lastsnd, lastrcv, lastack, rcv_space, notsent']
+            
+            data['cc_name'] = cc
             for item in stat:
+                if item.startswith(('wscale', 'mss', 'segs_out', 'segs_in', 
+                                  'send', 'lastsnd', 'lastrcv', 'lastack', 
+                                  'rcv_space', 'notsent')):
+                    continue
                 if item.startswith('bytes_acked:'):
-                    data[time][cc]['bytes_acked'] = int(item[item.rfind(':') + 1:])
+                    data['bytes_acked'] = int(item[item.rfind(':') + 1:])
                 elif item.startswith('retrans:'):
-                    data[time][cc]['retrans'] = int(item[item.rfind('/') + 1:])
+                    data['retrans'] = int(item[item.rfind('/') + 1:])
                 elif item.startswith('cwnd:'):
-                    data[time][cc]['cwnd'] = int(item[item.rfind(':') + 1:])
+                    data['cwnd'] = int(item[item.rfind(':') + 1:])
                 elif item.startswith('ssthresh:'):
-                    data[time][cc]['ssthresh'] = int(item[item.rfind(':') + 1:])
+                    data['ssthresh'] = int(item[item.rfind(':') + 1:])
                 elif item.startswith('data_segs_out:'):
-                    data[time][cc]['data_segs_out'] = int(item[item.rfind(':') + 1:])
+                    data['data_segs_out'] = int(item[item.rfind(':') + 1:])
                 elif item.startswith('rto:'):
-                    data[time][cc]['rto'] = (
+                    data['rto'] = (
                         float(item[item.find(':') + 1:item.rfind('/')]) / 1000)
                 elif item.startswith('rtt:'):
-                    data[time][cc]['rtt'] = (
+                    data['rtt'] = (
                         float(item[item.find(':') + 1:item.rfind('/')]) / 1000)
                 elif item.startswith('minrtt:'):
-                    data[time][cc]['minrtt'] = (
+                    data['minrtt'] = (
                         float(item[item.find(':') + 1:item.rfind('/')]) / 1000)
                 elif item.startswith('unacked:'):
-                    data[time][cc]['unacked'] = int(item[item.find(':') + 1:])
+                    data['unacked'] = int(item[item.find(':') + 1:])
                 elif item.startswith('pacing_rate'):
                     idx = stat.index('pacing_rate') + 1
-                    data[time][cc]['pacing_rate'] = float(re.split(r'M|K', stat[idx])[0])
+                    data['pacing_rate'] = float(re.split(r'M|K', stat[idx])[0])
                 elif item.startswith('bbr:'):
-                    data[time][cc]['bbr_bw'] = float(re.split(r'M|K', item.strip().split(',')[0].split(':')[2])[0])
-                    data[time][cc]['bbr_minrtt'] = (float(item.strip().split(',')[1].split(':')[1])/1000)
-                    data[time][cc]['bbr_pacing_gain'] = float(item.strip().split(',')[2].split(':')[1])
-                    data[time][cc]['bbr_cwnd_gain'] = float(item.strip().split(',')[3].split(':')[1].split(')')[0])
+                    data['bbr_bw'] = float(re.split(r'M|K', item.strip().split(',')[0].split(':')[2])[0])
+                    data['bbr_minrtt'] = (float(item.strip().split(',')[1].split(':')[1])/1000)
+                    data['bbr_pacing_gain'] = float(item.strip().split(',')[2].split(':')[1])
+                    data['bbr_cwnd_gain'] = float(item.strip().split(',')[3].split(':')[1].split(')')[0])
+                    
+            lst.append(data)
+            
                 
-                res = results.append(data[time][cc], ignore_index=True)
-                res[['port', 'time', 'cc_name']] = pd.DataFrame([[port,
-                                                        time, cc]], 
-                                                        index=res.index)
-                res = res[['port', 'time', 'cc_name', 'minrtt', 'retrans', 
-                           'rtt', 'rto', 'data_segs_out', 'cwnd', 'unacked', 
-                           'bbr_pacing_gain', 'bbr_minrtt', 'bbr_bw', 
-                           'bbr_cwnd_gain', 'bytes_acked', 'pacing_rate']]
+        res = pd.DataFrame(lst)
+
+        res = res[['port', 'time', 'cc_name', 'minrtt',  
+                    'rtt', 'rto', 'data_segs_out', 'cwnd', 'unacked', 
+                    'bbr_pacing_gain', 'bbr_minrtt', 'bbr_bw', 
+                    'bbr_cwnd_gain', 'bytes_acked', 'pacing_rate']]
                 
-            return data, res
+    return res
 
 
 if __name__ == '__main__':
-    
+        
     script_dir = os.path.dirname(os.path.realpath('ss.py'))
     rel_path = 'ss.log'
     log_file = os.path.join(script_dir, rel_path)
     
-    data, res = __read_sslog(log_file)
-    
+    res = __read_sslog(log_file)
+    fname = res['cc_name'][0]
     
     # Save output data to files
-    res.to_csv(path_or_buf=os.path.join(script_dir, 'results.csv'), 
-                   index=False) 
-    with open('data.txt', 'w') as outfile:
-        json.dump(data, outfile)
-    
+    res.to_csv(path_or_buf=os.path.join(script_dir, 'results-' +fname+'.csv'), 
+                    index=False) 
     
     
     
