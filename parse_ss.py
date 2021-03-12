0f82bc2713d544ead6c03faec0384793c22c20a9
Emilia Ndilokelwa Weyulu <eweyulu@mpi-inf.mpg.de>
2021-03-12 15:04:13 +0100

    Update log parser

--- parse_ss.py
+++ parse_ss.py
@@ -31,14 +31,21 @@ def __read_sslog(logfile):
             if line.startswith('State'):
                 continue
             
+            if line.startswith('SYN-SENT'):
+                tmp_item = line.strip().split()[0]
+                continue
+                               
             if line.startswith('ESTAB'):
                 
                 port = line.strip()
                 port = int(port[port.rfind(':') + 1:])
+                tmp_item = ''
                 continue
            
             if not line:
                 return None
+            if tmp_item == 'SYN-SENT':
+                continue
             data['time'] = time
             data['port'] = port
             stat = line.strip().split()
@@ -73,9 +80,9 @@ def __read_sslog(logfile):
                     data['unacked'] = int(item[item.find(':') + 1:])
                 elif item.startswith('pacing_rate'):
                     idx = stat.index('pacing_rate') + 1
-                    data['pacing_rate'] = float(re.split(r'M|K', stat[idx])[0])
+                    data['pacing_rate'] = float(re.split(r'M|K|b', stat[idx])[0])
                 elif item.startswith('bbr:'):
-                    data['bbr_bw'] = float(re.split(r'M|K', item.strip().split(',')[0].split(':')[2])[0])
+                    data['bbr_bw'] = float(re.split(r'M|K|b', item.strip().split(',')[0].split(':')[2])[0])
                     data['bbr_minrtt'] = (float(item.strip().split(',')[1].split(':')[1])/1000)
                     data['bbr_pacing_gain'] = float(item.strip().split(',')[2].split(':')[1])
                     data['bbr_cwnd_gain'] = float(item.strip().split(',')[3].split(':')[1].split(')')[0])
